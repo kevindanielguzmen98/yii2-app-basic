53af5cf379d6c1e45dc0f1569892c861cd1ac3a1
Kevin Guzman <kevindanielguzmen98@gmail.com>
2019-02-11 10:07:57 -0500

    Añadidos parámetros por defecto en params.

--- config/params.php
+++ config/params.php
@@ -2,4 +2,8 @@
 
 return [
     'adminEmail' => 'admin@example.com',
+    // Configuración de desarrolladores para generador de código
+    'developers' => [
+        'kevindanielguzmen98@gmail.com' => 'Kevin Daniel Guzmán Delgadillo'
+    ]
 ];
